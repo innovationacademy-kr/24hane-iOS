e5edec88e1a0f40128393830a23b216fdeb1213f
Katherine <01kathyy@gmail.com>
2023-02-14 16:40:57 +0900

    [add] population & notification #2

---
+++ HANE24/View/notificationView.swift
@@ -0,0 +1,20 @@
+//
+//  notificationView.swift
+//  HANE24
+//
+//  Created by Katherine JANG on 2/14/23.
+//
+
+import SwiftUI
+
+struct notificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct notificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        notificationView()
+    }
+}
