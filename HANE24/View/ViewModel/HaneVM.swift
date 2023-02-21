b4c7f0026ae78f1621f051a4e4df8d7ac8e69ac8
YunKi <yunki.dev@gmail.com>
2023-02-21 15:21:49 +0900

    [fixing] SigninWebView, , env.xcconfig

--- HANE24/View/ViewModel/HaneVM.swift
+++ HANE24/View/ViewModel/HaneVM.swift
@@ -12,27 +12,18 @@ class Hane: ObservableObject {
     @Published var monthlyAccumulationTime: Int = 0
     @Published var isSignedIn: Bool = false
     @Published var monthlyLogs: [String: [inOutLog]] = [:]
-    @Published var dailyTotalTimesInAMonth: [String: Int] = [:]
+    @Published var dailyTotalTimesInAMonth: [Int] = []
     
     var APIroot: String
     
-//    guard let infoDictionary: [String: Any] = Bundle.main.infoDictionary else {return}
-//    guard let apiRoot: String = infoDictionary["API_URL"] as? String else {return}
-//    APIroot = apiRoot
     init() {
         self.dailyAccumulationTime = 0
         self.monthlyAccumulationTime = 0
         self.isSignedIn = false
         self.monthlyLogs = [:]
-        self.dailyTotalTimesInAMonth = [:]
-        guard let infoDictionary: [String: Any] = Bundle.main.infoDictionary else {
-            self.APIroot = ""
-            return
-        }
-        guard let apiRoot: String = infoDictionary["API_URL"] as? String else {
-            self.APIroot = ""
-            return
-        }
-        self.APIroot = apiRoot
+        self.dailyTotalTimesInAMonth = []
+
+        self.APIroot = Bundle.main.infoDictionary?["API_URL"] as? String ?? "wrong"
+        print("self.APIroot = \(self.APIroot)")
     }
 }
