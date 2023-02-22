8dc7d5648ce0174a742c40d993a94ec82686c5db
Katherine <01kathyy@gmail.com>
2023-02-22 13:35:16 +0900

    [fix] SignInLogic #30

--- HANE24/View/SignInView.swift
+++ HANE24/View/SignInView.swift
@@ -7,55 +7,79 @@
 
 import SwiftUI
 
+enum Stat {
+    case buttonNotTabbed
+    case buttonTabbed
+    case viewAppeared
+    case readyToSignIn
+}
+
 struct SignInView: View {
     
-    @ObservedObject var hane : Hane
+    @EnvironmentObject var hane : Hane
+    @State var viewStat: Stat = .buttonNotTabbed
     
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.gradientPurple, .gradientBlue]), startPoint: .bottomLeading, endPoint: .topTrailing)
-                .ignoresSafeArea()
-            VStack(alignment: .center){
-                ZStack(alignment: .bottomLeading){
-                    Rectangle()
-                        .fill( LinearGradient(gradient: Gradient(colors: [.gradientPurple, .gradientBlue]), startPoint: .bottomLeading, endPoint: .topTrailing))
-                        .frame(width: 150, height: 150)
-                    VStack(alignment: .leading){
-                        Text("24")
-                            .font(.system(size: 35, weight: .semibold))
-                        Text("HANE")
-                            .font(.system(size: 35, weight: .heavy))
-                    }
-                    .padding(10)
-                    .foregroundColor(.white)
-                }
-                .padding(.bottom, 70)
-                Text("입실과 퇴실의 짝이 일치하는 경우에만 출입 누적시간이 반영됩니다.")
-                    .foregroundColor(.white)
-                    .font(.system(size: 12, weight: .regular))
-                Text("입/퇴실 시 출입카드를 꼭 태깅해주세요.")
-                    .font(.system(size: 12, weight: .regular))
-                    .foregroundColor(.white)
-                Button{
-                    hane.status = .loadWebView
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(Color.DarkDefaultBG)
-                        Text("LOG IN")
+            SignInWebView(viewStat: $viewStat)
+            if viewStat != .readyToSignIn{
+                ZStack{
+                    LinearGradient(gradient: Gradient(colors: [.gradientPurple, .gradientBlue]), startPoint: .bottomLeading, endPoint: .topTrailing)
+                        .ignoresSafeArea()
+                    VStack(alignment: .center){
+                        ZStack(alignment: .bottomLeading){
+                            Rectangle()
+                                .fill( LinearGradient(gradient: Gradient(colors: [.gradientPurple, .gradientBlue]), startPoint: .bottomLeading, endPoint: .topTrailing))
+                                .frame(width: 150, height: 150)
+                            VStack(alignment: .leading){
+                                Text("24")
+                                    .font(.system(size: 35, weight: .semibold))
+                                Text("HANE")
+                                    .font(.system(size: 35, weight: .heavy))
+                            }
+                            .padding(10)
                             .foregroundColor(.white)
+                        }
+                        .padding(.bottom, 70)
+                        Text("입실과 퇴실의 짝이 일치하는 경우에만 출입 누적시간이 반영됩니다.")
+                            .foregroundColor(.white)
+                            .font(.system(size: 12, weight: .regular))
+                        Text("입/퇴실 시 출입카드를 꼭 태깅해주세요.")
+                            .font(.system(size: 12, weight: .regular))
+                            .foregroundColor(.white)
+                        Button{
+                            if self.viewStat == .viewAppeared {
+                                self.viewStat = .readyToSignIn
+                            } else {
+                                self.viewStat = .buttonTabbed
+                            }
+                        } label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .foregroundColor(Color.DarkDefaultBG)
+                                if viewStat != .buttonTabbed {
+                                    Text("LOG IN")
+                                        .foregroundColor(.white)
+                                }
+                                else {
+                                    /// [FixMe] ProgressView -> Progress[Dot] with Gradient => passthroughSubject??
+                                    ProgressView()
+                                        .foregroundColor(.white)
+                                }
+                            }
+                        }
+                        .frame(height: 45)
+                        .padding(10)
                     }
+                    .padding(.horizontal, 10)
                 }
-                .frame(height: 45)
-                .padding(10)
             }
-            .padding(.horizontal, 10)
         }
     }
 }
 
 struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
-        SignInView(hane: Hane())
+        SignInView()
     }
 }
--- HANE24/View/ViewModel/HaneVM.swift
+++ HANE24/View/ViewModel/HaneVM.swift
@@ -20,6 +20,7 @@ class Hane: ObservableObject {
     @Published var dailyAccumulationTime: Int64 = 0
     @Published var monthlyAccumulationTime: Int64 = 0
     @Published var status: Status = .beforeSignIn
+    @Published var isSignIn: Bool = false
     @Published var monthlyLogs: [Date: [InOutLog]] = [:]
     @Published var dailyTotalTimesInAMonth: [Int64] = Array(repeating: 0, count: 32)
     
@@ -34,6 +35,7 @@ class Hane: ObservableObject {
         self.dailyAccumulationTime = 0
         self.monthlyAccumulationTime = 0
         self.status = .beforeSignIn
+        self.isSignIn = false
         self.monthlyLogs = [:]
         self.dailyTotalTimesInAMonth = []
         
@@ -146,7 +148,7 @@ class Hane: ObservableObject {
                     }
                 })
         UserDefaults.standard.removeObject(forKey: "Token")
-        self.status = .beforeSignIn
+        self.isSignIn = false
     }
 }
 
