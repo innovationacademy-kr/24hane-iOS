e5eaa7a6b1fa0ec895aceb2f65f9dd3a40332c32
YunKi <yunki.dev@gmail.com>
2023-03-03 20:29:32 +0900

    [fix] detail, API v1 > v2

--- HANE24/View/Home/PopulationView.swift
+++ HANE24/View/Home/PopulationView.swift
@@ -22,13 +22,13 @@ struct PopulationView: View {
                     RoundedRectangle(cornerRadius: 20)
                         .foregroundColor(.white)
                         .frame(width: 155, height: 80)
-                    HStack{
+                    HStack(alignment: .bottom) {
                         Text("개포")
                             .font(.system(size: 16, weight: .semibold))
                             .padding(.trailing, 20)
                             .foregroundColor(.black)
                         Text("\(hane.clusterPopulation.gaepo)")
-                            .font(.system(size: 18, weight: .semibold))
+                            .font(.system(size: 20, weight: .semibold))
                             .foregroundColor(.black)
                             .padding(.leading, 20)
                         Text("명")
@@ -40,13 +40,13 @@ struct PopulationView: View {
                     RoundedRectangle(cornerRadius: 20)
                         .foregroundColor(.white)
                         .frame(width: 155, height: 80)
-                    HStack{
+                    HStack(alignment: .bottom) {
                         Text("서초")
                             .font(.system(size: 16, weight: .semibold))
                             .padding(.trailing, 20)
                             .foregroundColor(.black)
                         Text("\(hane.clusterPopulation.seocho)")
-                            .font(.system(size: 18, weight: .semibold))
+                            .font(.system(size: 20, weight: .semibold))
                             .foregroundColor(.black)
                             .padding(.leading, 20)
                         Text("명")
