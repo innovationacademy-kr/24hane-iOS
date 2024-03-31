097d4b67f04fea6b564539dfb7275266416d3c34
HiHoi <ghtjd5087@gmail.com>
2024-03-31 22:26:12 +0900

    feat: calendar test add

--- HANE24TEST/HANE24Test.swift
+++ HANE24TEST/HANE24Test.swift
@@ -36,6 +36,9 @@ let missingJsonData = """
 
 let missingAnswer = [Log(inTime: Optional("18:08:31"), outTime: nil, logTime: Optional("누락"))]
 
+let emptyMonthlyData = PerMonth(login: "hoslim", profileImage: "https://cdn.intra.42.fr/users/8f20c45abe403f1fd56b58211f819bea/hoslim.jpg", inOutLogs: [], totalAccumulationTime: 0, acceptedAccumulationTime: 0)
+
+// 2022-12-30일자 기록
 let monthlyJsonData = """
   {
 	"login": "hoslim",
@@ -52,39 +55,7 @@ let monthlyJsonData = """
   }
 """.data(using: .utf8)!
 
-
-
-let emptyMonthlyData = PerMonth(login: "hoslim", profileImage: "https://cdn.intra.42.fr/users/8f20c45abe403f1fd56b58211f819bea/hoslim.jpg", inOutLogs: [], totalAccumulationTime: 0, acceptedAccumulationTime: 0)
-
-//let utcAnswer = [Log(inTime: Optional("03:14:39"), outTime: Optional("07:42:19"), logTime: Optional("19:27:40"))]
 let answer = [Log(inTime: Optional("12:14:39"), outTime: Optional("16:42:19"), logTime: Optional("04:27:40"))]
-
-/**
-
- 테스트해야 하는 함수 및 기능
-
- # 캘린더
- 1. updateMonthlyLogs 테스트
- 2. callPerMonth 테스트
- 3. 누락 및 상태에 대한 테스트
-
- # 테스트 구현시 원칙
- 1. Given
- 2. When
- 3. Then
-
- */
-
-//protocol MockNetworkInfoProtocol {
-//	var session: URLSession { get set }
-//	var apiRoot: String { get set }
-//}
-//
-//protocol MockNetworkGetProtocol {
-//	func getRequest<T>(_ urlPath: String, type: T.Type) async throws -> T? where T : Decodable
-//}
-//
-//protocol MockNetworkProtocol: MockNetworkProtocol {}
 
 class MockNetwork: NetworkProtocol {
 	static var shared = MockNetwork()
@@ -181,31 +152,17 @@ final class HaneCalendarTest: XCTestCase {
 	// MARK: - Daily Data Test
 	func testSeperateDailyData() async {
 		do {
-			try await sut.updateMonthlyLogs(date: .now)
+			sut.calendarModel.selectedDate = Date(timeIntervalSince1970: 1672358400)
+			try await sut.updateMonthlyLogs(date: Date(timeIntervalSince1970: 1672358400))
 		} catch {
 			XCTFail("testSeperateDailyData data load is failed")
 		}
 		let parsedData = sut.convertedSelectedMonthlyLog
-		print(parsedData)
 
 		// then
-//		XCTAssertEqual(parsedData, answer[0])
+		XCTAssertEqual(parsedData, answer)
 	}
 
-//	// MARK: - Missing Data Test
-//	func testMissingDailyData() {
-//		// 질문사항 3. 이 부분을 테스트 하려면 convert 함수 내 hane의 selectedDate를 사용하는데 이 용도와 계산 원리
-//		do {
-//			let data = try JSONDecoder().decode(PerMonth.self, from: missingJsonData)
-//			sut.perMonth = data
-//		} catch {
-//			XCTFail("JSON Parse Fail")
-//			return
-//		}
-//
-//		calendarView.$hane.selectedDate = Date.now
-//		let parsedData = calendarView.convert(sut.perMonth.inOutLogs)
-//
-//		XCTAssertEqual(parsedData, missingAnswer)
-//	}
+	// MARK: - Missing Data Test
+	
 }
