4e79f163fd0c08614de910319035df08a1fc7b7a
HiHoi <ghtjd5087@gmail.com>
2024-04-05 13:55:05 +0900

    feat: reissuevm - finish / get / request add

--- HANE24/ViewModel/ReissueVM.swift
+++ HANE24/ViewModel/ReissueVM.swift
@@ -7,25 +7,65 @@
 
 import Foundation
 
-protocol ReissueProtocol {}
+protocol ReissueInfomationProtocol {
+	var loading: Bool { get set }
+}
+
+protocol ReissueNetworkProtocol {
+	// 재발급 신청
+	func requestReissue() async throws
+	// 재발급이 끝났는지를 확인
+	func finishReissue() async throws
+	// 재발급 상태를 확인
+	func getReissueStatus() async throws
+
+	init(network: NetworkProtocol)
+}
+
+protocol ReissueProtocol: 
+	ObservableObject,
+	ReissueInfomationProtocol,
+	ReissueNetworkProtocol
+{}
 
 class ReissueVM: ReissueProtocol {
 	private let network: NetworkProtocol
 	@Published var loading: Bool = false
+	@Published var cardReissueState: ReissueState = .init(state: "")
 
-	@MainActor
 	func requestReissue() async throws {
 		self.loading = true
 		let url = URL(string: "/v2/reissue/request")!
 		do {
 			try await network.postRequest(url.absoluteString)
 		} catch {
-
+			throw MyError.tokenExpired("get new token!")
 		}
 		self.loading = false
 	}
 
-	init(network: NetworkProtocol = NetworkManager.shared) {
+	func finishReissue() async throws {
+		self.loading = true
+		let url = URL(string: "/v2/reissue/finish")!
+		do {
+			try await network.patchRequest(url.absoluteString)
+		} catch {
+			throw MyError.tokenExpired("get new token!")
+		}
+		self.loading = false
+	}
+
+	func getReissueStatus() async throws {
+		self.loading = true
+		let url = URL(string: "/v2/reissue")!
+		guard let state = try await network.getRequest(url.absoluteString, type: ReissueState.self) else {
+			fatalError("ReissueVM - getReissueStatus")
+		}
+		self.cardReissueState = state
+		self.loading = false
+	}
+
+	required init(network: NetworkProtocol = NetworkManager.shared) {
 		self.network = network
 	}
 }
---
+++ HANE24TEST/FakeNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  FakeNetwork.swift
+//  HANE24Test
+//
+//  Created by Hosung Lim on 4/5/24.
+//
+
+import Foundation
