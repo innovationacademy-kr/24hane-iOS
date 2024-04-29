140eeaac504868a4a7519179afa2b8a279b7fd48
HiHoi <ghtjd5087@gmail.com>
2024-04-29 17:45:38 +0900

    feat: setting View add

--- HANE24Watch/View/SettingView.swift
+++ HANE24Watch/View/SettingView.swift
@@ -7,22 +7,59 @@
 
 import SwiftUI
 
-struct SettingView: View {
+struct TokenTest: View {
     @StateObject var watch = WatchToiOSConnect()
     var body: some View {
-		VStack {
-			Text("설정")
-            Text("토큰: \(watch.token)")
-            Button {
-                watch.token = ""
-            } label: {
-                Text("초기화")
-            }
-		}
+        Text("토큰: \(watch.token)")
+        Button {
+            watch.token = ""
+        } label: {
+            Text("초기화")
+        }
         .onAppear {
             Task {
                 watch.requestDataFromiOS()
             }
+        }
+    }
+}
+
+struct DisplaySettingView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct AboutAppView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct SettingView: View {
+    let settingItems: [(String, String, () -> AnyView)] = [
+        ("토큰 확인", "person.slash", { AnyView(TokenTest()) }),
+        ("화면 설정", "gearshape.fill", { AnyView(DisplaySettingView()) }),
+        ("정보", "info.circle.fill", { AnyView(AboutAppView()) }),
+        ("로그아웃", "person.fill", { AnyView(EmptyView()) })
+    ]
+    
+    @State private var selectedIndex: Int?
+    
+    var body: some View {
+        NavigationStack {
+            List(Array(settingItems.enumerated()), id: \.offset) { index, item in
+                NavigationLink(destination: {
+                    item.2()
+                }, label: {
+                    Label {
+                        Text(item.0)
+                    } icon: {
+                        Image(systemName: item.1)
+                    }
+                })
+            }
+            .navigationTitle("설정")
         }
     }
 }
