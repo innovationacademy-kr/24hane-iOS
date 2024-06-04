9f63bca8130662896b10efc9f46d191666a74e2d
HiHoi <ghtjd5087@gmail.com>
2024-06-04 17:54:39 +0900

    fix: setter is added

--- HANE24Watch/ViewModel/SettingViewModel.swift
+++ HANE24Watch/ViewModel/SettingViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol DisplaySettingDataProtocol {
-    var displaySettig: DisplaySettingModel { get }
+    var displaySettig: DisplaySettingModel { get set }
 }
 
 protocol DisplaySettingLogicProtocol {
